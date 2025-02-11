7bf02ec4552b4e7157273de62af537c833dfba41
Sainnhe Park <i@sainnhe.dev>
2025-02-11 20:18:49 +0800

    fix(log): add mutex

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,9 +1,13 @@
 // Package log implements a common logging interface.
 package log
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 var defaultLogger Logger
+var mu sync.Mutex
 
 // ProvideLogger initializes a logger based on Config and provides logger dependency. It'll also set this logger as the
 // default logger.
@@ -27,17 +31,21 @@ func ProvideLogger(cfg *Config) (logger Logger, cleanup func(), err error) {
 
 // GetDefault gets the default logger.
 func GetDefault() Logger {
+	mu.Lock()
+	defer mu.Unlock()
 	// Return default logger if it exists.
 	if defaultLogger != nil {
 		return defaultLogger
 	}
 	// Otherwise set default logger and return it.
-	SetDefault(nil)
-	return GetDefault()
+	defaultLogger = NewLight()
+	return defaultLogger
 }
 
 // SetDefault sets the default logger.
 func SetDefault(logger Logger) {
+	mu.Lock()
+	defer mu.Unlock()
 	if logger == nil {
 		defaultLogger = NewLight()
 	} else {
