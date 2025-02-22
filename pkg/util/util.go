7a2927fe21c4b43519f2bcc8ba2ab141b22a7296
Sainnhe Park <i@sainnhe.dev>
2025-02-22 22:34:49 +0800

    refactor: remove util.MergeErrors

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -2,10 +2,8 @@
 package util
 
 import (
-	"errors"
 	"fmt"
 	"runtime/debug"
-	"strings"
 
 	"github.com/teamsorghum/go-common/pkg/log"
 )
@@ -20,39 +18,4 @@ func Recover() {
 // ToStr converts a variable to a string.
 func ToStr(v any) string {
 	return fmt.Sprintf("%+v", v)
-}
-
-/*
-MergeErrors merges multiple errors and returns a merged error.
-
-The logic of merge is:
-  - If all the incoming errors are nil, return nil.
-  - If 1 of the incoming errors is not nil, return this error.
-  - If more than 1 of the incoming errors are not nil, then merge the error message and return.
-
-Params:
-  - errs ...error: Errors to be merged.
-
-Returns:
-  - error: Merged error.
-*/
-func MergeErrors(errs ...error) error {
-	nonNilErrors := []error{}
-	for _, err := range errs {
-		if err != nil {
-			nonNilErrors = append(nonNilErrors, err)
-		}
-	}
-	switch len(nonNilErrors) {
-	case 0:
-		return nil
-	case 1:
-		return nonNilErrors[0]
-	default:
-		msgs := make([]string, len(nonNilErrors))
-		for i, err := range nonNilErrors {
-			msgs[i] = err.Error()
-		}
-		return errors.New(strings.Join(msgs, "; "))
-	}
 }
--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -1,7 +1,6 @@
 package util_test
 
 import (
-	"errors"
 	"testing"
 
 	"github.com/teamsorghum/go-common/pkg/util"
@@ -48,63 +47,6 @@ func TestToStr(t *testing.T) {
 			got := util.ToStr(tt.input)
 			if got != tt.want {
 				t.Errorf("Want %q, got %q", tt.want, got)
-			}
-		})
-	}
-}
-
-func TestMergeErrors(t *testing.T) {
-	t.Parallel()
-
-	err1 := errors.New("error one")
-	err2 := errors.New("error two")
-	err3 := errors.New("error three")
-
-	tests := []struct {
-		name string
-		errs []error
-		want error
-	}{
-		{
-			name: "All nil errors",
-			errs: []error{nil, nil, nil},
-			want: nil,
-		},
-		{
-			name: "No errors",
-			errs: []error{},
-			want: nil,
-		},
-		{
-			name: "Single error",
-			errs: []error{err1},
-			want: err1,
-		},
-		{
-			name: "Multiple errors",
-			errs: []error{err1, err2, err3},
-			want: errors.New("error one; error two; error three"),
-		},
-		{
-			name: "Mixed nil and non-nil errors",
-			errs: []error{err1, nil, nil},
-			want: err1,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			t.Parallel()
-
-			got := util.MergeErrors(tt.errs...)
-			if tt.want == nil && got != nil {
-				t.Errorf("Want nil error, got %q", got)
-			} else if tt.want != nil {
-				if got == nil {
-					t.Errorf("Want error %q, got nil", tt.want)
-				} else if got.Error() != tt.want.Error() {
-					t.Errorf("Want error %q, got %q", tt.want.Error(), got.Error())
-				}
 			}
 		})
 	}
