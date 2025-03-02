587e70a7a60ece21eeab6d3b80cfc3348906bd4f
Sainnhe Park <i@sainnhe.dev>
2025-03-02 22:12:06 +0800

    style(limiter): add new line between config fields

--- pkg/limiter/config.go
+++ pkg/limiter/config.go
@@ -3,35 +3,35 @@ package limiter
 // RateLimitConfig defines the rate limit config.
 type RateLimitConfig struct {
 	// Enable indicates whether rate limit is enabled.
-	Enable bool `json:"enable" yaml:"enable" env:"LimiterRateLimitEnable" default:"true"`
+	Enable bool `json:"enable" yaml:"enable" env:"LIMITER_RATE_LIMIT_ENABLE" default:"true"`
 
 	// Prefix is the prefix for keys, which can be used to describe current business and avoid conflicts.
-	Prefix string `json:"prefix" yaml:"prefix" env:"LimiterRateLimitPrefix" default:"*"`
+	Prefix string `json:"prefix" yaml:"prefix" env:"LIMITER_RATE_LIMIT_PREFIX" default:"*"`
 
 	// Limit is the limit of requests in a given time window.
-	Limit int `json:"limit" yaml:"limit" env:"LimiterRateLimitLimit" default:"1"`
+	Limit int `json:"limit" yaml:"limit" env:"LIMITER_RATE_LIMIT_LIMIT" default:"1"`
 
 	// WindowMs is the time window for measurement in milliseconds.
-	WindowMs int `json:"window_ms" yaml:"window_ms" env:"LimiterRateLimitWindowMs" default:"1000"`
+	WindowMs int `json:"window_ms" yaml:"window_ms" env:"LIMITER_RATE_LIMIT_WINDOW_MS" default:"1000"`
 }
 
 // PeakShavingConfig defines the peak shaving config.
 type PeakShavingConfig struct {
 	// Enable indicates whether peak shaving is enabled.
-	Enable bool `json:"enable" yaml:"enable" env:"LimiterPeakShavingEnable" default:"true"`
+	Enable bool `json:"enable" yaml:"enable" env:"LIMITER_PEAK_SHAVING_ENABLE" default:"true"`
 
 	// Prefix is the prefix for keys, which can be used to describe current business and avoid conflicts.
-	Prefix string `json:"prefix" yaml:"prefix" env:"LimiterPeakShavingPrefix" default:"*"`
+	Prefix string `json:"prefix" yaml:"prefix" env:"LIMITER_PEAK_SHAVING_PREFIX" default:"*"`
 
 	// Limit is the limit of requests in a given time window.
-	Limit int `json:"limit" yaml:"limit" env:"LimiterPeakShavingLimit" default:"1"`
+	Limit int `json:"limit" yaml:"limit" env:"LIMITER_PEAK_SHAVING_LIMIT" default:"1"`
 
 	// WindowMs is the time window for measurement in milliseconds.
-	WindowMs int `json:"window_ms" yaml:"window_ms" env:"LimiterPeakShavingWindowMs" default:"1000"`
+	WindowMs int `json:"window_ms" yaml:"window_ms" env:"LIMITER_PEAK_SHAVING_WINDOW_MS" default:"1000"`
 
 	// MaxAttempts is the maximum number of attempts.
-	MaxAttempts int `json:"max_attempts" yaml:"max_attempts" env:"LimiterPeakShavingMaxAttempts" default:"10"`
+	MaxAttempts int `json:"max_attempts" yaml:"max_attempts" env:"LIMITER_PEAK_SHAVING_MAX_ATTEMPTS" default:"10"`
 
 	// AttemptIntervalMs is the interval between each attempt in milliseconds.
-	AttemptIntervalMs int `json:"attempt_interval_ms" yaml:"attempt_interval_ms" env:"LimiterPeakShavingAttemptIntervalMs" default:"500"` // nolint:lll
+	AttemptIntervalMs int `json:"attempt_interval_ms" yaml:"attempt_interval_ms" env:"LIMITER_PEAK_SHAVING_ATTEMPT_INTERVAL_MS" default:"500"` // nolint:lll
 }
