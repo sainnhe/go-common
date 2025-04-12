2e70e4ceea01fc1b649054741de56572be5b1fb0
Sainnhe Park <i@sainnhe.dev>
2025-04-12 18:59:16 +0800

    feat(constant): add ErrCodeUnknown and Day

--- pkg/constant/errors.go
+++ pkg/constant/errors.go
@@ -7,5 +7,10 @@ package constant
 
 import "errors"
 
+const (
+	// ErrCodeUnknown indicates an unknown error.
+	ErrCodeUnknown = 1
+)
+
 // ErrNilDeps indicates that there exists nil dependencies.
 var ErrNilDeps = errors.New("nil dependencies")
---
+++ pkg/constant/time.go
@@ -0,0 +1,6 @@
+package constant
+
+import "time"
+
+// Day is the duration for a day.
+var Day = time.Duration(24) * time.Hour // nolint:mnd
