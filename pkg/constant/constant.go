3634eb262f0e2908fdd847b5abf130c573c488a1
Sainnhe Park <i@sainnhe.dev>
2025-04-13 16:36:58 +0800

    feat(constant): add TimeFormatYearAndMonth

--- pkg/constant/constant.go
+++ pkg/constant/constant.go
@@ -5,3 +5,8 @@
 
 // Package constant defines some constants.
 package constant
+
+const (
+	// TimeFormatYearMonth defines the time format that only contains year and month.
+	TimeFormatYearMonth = "2006-01"
+)
